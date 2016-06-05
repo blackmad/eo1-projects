058d8b33b653d022a545a16387ffa4aa2ee48862
David Blackman <david@blackmad.com>
2016-06-05 11:13:53 -0400

    nocache?

--- main.py
+++ main.py
@@ -8,6 +8,22 @@ from flask import Flask, request, send_from_directory, render_template
 import logging
 logging.basicConfig()
 
+from flask import make_response
+from functools import wraps, update_wrapper
+from datetime import datetime
+
+def nocache(view):
+    @wraps(view)
+    def no_cache(*args, **kwargs):
+        response = make_response(view(*args, **kwargs))
+        response.headers['Last-Modified'] = datetime.now()
+        response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0, max-age=0'
+        response.headers['Pragma'] = 'no-cache'
+        response.headers['Expires'] = '-1'
+        return response
+        
+    return update_wrapper(no_cache, view)
+
 
 from eo_python.eo import ElectricObject, get_credentials
 
@@ -25,9 +41,11 @@ def set_text_form():
 
 @app.route('/eo1/set_text', methods=['POST'])
 def set_text():
-  eo.set_url('http://eo1.blackmad.com:5222/app/text/chunk/chunk2.html?text=' + request.form['text'])
+  # eo.set_url('http://eo1.blackmad.com:5222/app/text/chunk/chunk2.html?text=' + request.form['text'])
+  eo.set_url('http://eo1.blackmad.com/app/text/chunk/chunk2.html?text=you%20should%20test%20this!')
   return 'okay, cool, hope that worked'
 
+@nocache
 @app.route('/app/<path:path>')
 def send_app(path):
  print 'trying to send ' + path
