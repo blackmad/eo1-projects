7521a482a1617b3ca299d8389f99966b01e8b68b
David Blackman <david@blackmad.com>
2017-07-10 21:18:12 -0400

    app.py rename

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-web gunicorn manage:app
+web gunicorn app:app
 
--- app.py
+++ app.py
@@ -57,4 +57,4 @@ def home(url):
   return Response(stream_with_context(req.iter_content()), content_type = req.headers['content-type'])
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=5222, threaded=True)
+  app.run(debug=True, use_reloader=True)
