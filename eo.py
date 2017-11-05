56413893a20a0b645d5f37c4cea81b0175fa7364
David Blackman <david@blackmad.com>
2017-11-05 16:50:32 -0500

    commandline url setter

---
+++ eo.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+from eo_python.eo import ElectricObject, get_credentials
+
+import requests
+
+credentials = get_credentials()
+print credentials
+eo = ElectricObject(username=credentials["username"], password=credentials["password"])
+
+import sys
+print sys.argv[1]
+eo.set_url(sys.argv[1])
